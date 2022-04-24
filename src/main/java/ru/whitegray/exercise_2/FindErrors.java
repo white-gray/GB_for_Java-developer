13243c7d458629b7347ae6d81dcf4cc6230b3c9c
WhiteGray <WhiteGray-Black@yandex.ru>
2022-04-24 13:46:18 +0300

    PfI_HW1

---
+++ src/main/java/ru/whitegray/exercise_2/FindErrors.java
@@ -0,0 +1,71 @@
+package ru.whitegray.exercise_2;
+
+/*
+Былишибки в коде
+- отсутствует interface Engine
+- в классе class Lorry интерфейсы Moveable, Stopable надо implements,
+    и так, как есть extends абстрактный класс Car с абстрактным методом Open, надо этот метод переопределить
+* */
+
+ class FindErrors {
+    interface Moveable {
+        void move();
+    }
+    interface Stopable {
+        void stop();
+    }
+
+    interface Engine {}
+
+    abstract class Car {
+        public Engine engine;
+        private String color;
+        private String name;
+        protected void start() {
+            System.out.println("Car starting");
+        }
+        abstract void open();
+        public Engine getEngine() {
+            return engine;
+        }
+        public void setEngine(Engine engine) {
+            this.engine = engine;
+        }
+        public String getColor() {
+            return color;
+        }
+        public void setColor(String color) {
+            this.color = color;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+    class LightWeightCar extends Car implements Moveable {
+        @Override
+        void open() {
+            System.out.println("Car is open");
+        }
+        @Override
+        public void move() {
+            System.out.println("Car is moving");
+        }
+    }
+    class Lorry extends Car implements Moveable, Stopable {
+        public void move(){
+            System.out.println("Car is moving");
+        }
+        public void stop(){
+            System.out.println("Car is stop");
+        }
+
+        @Override
+        void open() {
+            System.out.println("Car is open");
+        }
+    }
+
+}
